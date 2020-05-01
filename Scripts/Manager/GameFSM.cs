9e020301b0f0ee05257d5f3528ef0b36feeb3c40
narasu <narasu@users.noreply.github.com>
2020-05-01 13:45:07 +0200

    First commit

---
+++ Scripts/Manager/GameFSM.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFSM
+{
+    
+    private Dictionary<GameStateType, GameState> states;
+
+    public GameStateType CurrentStateType { get; private set; }
+    private GameState currentState;
+    private GameState previousState;
+
+    public void Initialize()
+    {
+        states = new Dictionary<GameStateType, GameState>();
+
+        states.Add(GameStateType.MainMenu, new MainMenuState());
+        states.Add(GameStateType.Play, new PlayState());
+        states.Add(GameStateType.Pause, new PauseState());
+        states.Add(GameStateType.Win, new WinState());
+        states.Add(GameStateType.Dead, new DeadState());
+    }
+
+    public void UpdateState()
+    {
+        currentState?.Update();
+    }
+
+    public void GotoState(GameStateType key)
+    {
+        if (!states.ContainsKey(key))
+        {
+            return;
+        }
+
+        currentState?.Exit();
+
+        previousState = currentState;
+        CurrentStateType = key;
+        currentState = states[CurrentStateType];
+
+        currentState.Enter();
+    }
+
+    public GameState GetState(GameStateType type)
+    {
+        // returns state if it exists, 
+        // else null
+        /*
+        if (states.ContainsKey(type))
+            return states[type];
+        else
+            return null;
+            */
+        
+        //ik ben eigenwijs
+        if (!states.ContainsKey(type))
+        {
+            return null;
+        }
+        return states[type];
+    }
+}
---
+++ Scripts/Manager/GameState.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStateType { MainMenu, Play, Pause, Win, Dead }
+
+public abstract class GameState
+{
+    public abstract void Enter();
+    public abstract void Update();
+    public abstract void Exit();
+}
+
+public class MainMenuState : GameState 
+{
+    public override void Enter()
+    {
+        //open main menu, freeze game, unlock cursor
+        GameManager.Instance.mainMenuObject.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+    }
+    public override void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+    public override void Exit()
+    {
+        //close menu, unfreeze game
+        GameManager.Instance.mainMenuObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
+public class PlayState : GameState
+{
+    public override void Enter()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        GameManager.Instance.trapTextObject.SetActive(true);
+    }
+    public override void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameManager.Instance.GotoPause();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Player.Instance?.SetTrap();
+        }
+
+        GameManager.Instance.trapText.text = "Traps: " + Player.Instance.CurrentTraps.ToString();
+    }
+    public override void Exit()
+    {
+        GameManager.Instance.trapTextObject.SetActive(false);
+    }
+}
+public class PauseState : GameState
+{
+    public override void Enter()
+    {
+        //open pause menu, freeze game, unlock cursor
+        GameManager.Instance.pauseObject.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public override void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            GameManager.Instance.GotoPlay();
+    }
+    public override void Exit()
+    {
+        GameManager.Instance.pauseObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
+public class WinState : GameState
+{
+    public override void Enter()
+    {
+        GameManager.Instance.winMenuObject.SetActive(true);
+        GameManager.Instance.EndLevel();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0f;
+    }
+    public override void Update()
+    {
+
+    }
+    public override void Exit()
+    {
+        GameManager.Instance.winMenuObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
+public class DeadState : GameState
+{
+    public override void Enter()
+    {
+        GameManager.Instance.deadMenuObject.SetActive(true);
+        GameManager.Instance.EndLevel();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0f;
+    }
+    public override void Update()
+    {
+
+    }
+    public override void Exit()
+    {
+        GameManager.Instance.deadMenuObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
