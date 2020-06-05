fe477c31746b33773466ae019180aa7c33ca999b
narasu <narasu@users.noreply.github.com>
2020-06-05 14:24:26 +0200

    added music. needs to be reverted

--- Assets/Scripts/Interactables/LockItem.cs
+++ Assets/Scripts/Interactables/LockItem.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class LockItem : MonoBehaviour
 {
+
+    [FMODUnity.EventRef]
+    public string UnlockEvent = "";
+
+    FMOD.Studio.EventInstance unlockEventInstance;
+
     private bool unlocked = false;
+
+    private void Awake()
+    {
+        unlockEventInstance = FMODUnity.RuntimeManager.CreateInstance(UnlockEvent);
+    }
     public void Unlock()
     {
         if (unlocked)
             return;
 
         Debug.Log("Unlocked");
+        unlockEventInstance.start();
         unlocked = true;
     }
 }
