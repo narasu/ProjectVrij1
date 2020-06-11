3eeffd8958ad24bf9f5da63508a315adc4bfea39
narasu <narasu@users.noreply.github.com>
2020-06-11 14:05:05 +0200

    Added working camera prop, tutorial text, some fixes to interaction code

---
+++ Assets/Scripts/Interactables/CameraProp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraProp : Interactable
+{
+    [SerializeField] private GameObject tutorialText;
+
+    public void EnableText()
+    {
+        tutorialText.SetActive(true);
+    }
+
+    public void DisableText()
+    {
+        tutorialText.SetActive(false);
+    }
+
+    public override void HandleInteraction()
+    {
+        base.HandleInteraction();
+        Player.Instance.GetCamera();
+        Destroy(gameObject);
+    }
+}
