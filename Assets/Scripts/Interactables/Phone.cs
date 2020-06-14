ff319616afffc27d55f64e3df205f09699741536
narasu <narasu@users.noreply.github.com>
2020-06-14 19:00:04 +0200

    phone script

---
+++ Assets/Scripts/Interactables/Phone.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phone : KeyItem
+{
+    //private MeshRenderer meshRenderer;
+    private Rigidbody rb;
+
+    [SerializeField] private GameObject phoneAudio;
+    private SwitchableAudio switchableAudio;
+    private Movable movable;
+    private KeyItem keyItem;
+
+
+    private void Awake()
+    {
+        //meshRenderer = GetComponent<MeshRenderer>();
+        rb = GetComponent<Rigidbody>();
+        movable = GetComponent<Movable>();
+        keyItem = GetComponent<KeyItem>();
+
+        //switchableAudio = GetComponent<SwitchableAudio>();
+
+        rb.isKinematic = true;
+
+        switchableAudio = phoneAudio.GetComponent<SwitchableAudio>();
+        
+    }
+
+    private void Start()
+    {
+        movable.enabled = false;
+        keyItem.enabled = false;
+    }
+
+    private void Update()
+    {
+        phoneAudio.transform.position = transform.position;
+        
+    }
+
+    public void Activate()
+    {
+
+        //gets enabled after timer elapsed
+        //start playing sound using SwitchableAudio script
+        switchableAudio.gameObject.SetActive(true);
+
+
+        //activate movable and KeyItem components
+        movable.enabled = true;
+        keyItem.enabled = true;
+        rb.isKinematic = false;
+    }
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+        if (other == lockTrigger)
+        {
+            Destroy(switchableAudio);
+        }
+    }
+}
