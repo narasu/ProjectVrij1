c969166053daf64c8224270f4016ad76f93f5839
Pheraone <brechje@me.com>
2020-06-15 22:10:17 +0200

    Fish boye follows you

---
+++ Assets/Scripts/FishyBoye/FishMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FishMovement : MonoBehaviour
+{
+    [SerializeField]
+    Transform destination;
+    
+    NavMeshAgent navMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        
+    }
+   private void SetDestination()
+    {
+        if(destination != null)
+        {
+            Vector3 targetVector = destination.transform.position;
+            Debug.Log("interessant");
+            navMeshAgent.SetDestination(targetVector);
+        }
+    }
+
+    private void Update()
+    {
+        if (navMeshAgent == null)
+        {
+            Debug.LogError("Not working m8");
+        }
+        else
+        {
+            SetDestination();
+        }
+    }
+
+}
