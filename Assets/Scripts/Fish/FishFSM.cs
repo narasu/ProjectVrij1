646626a15bf44e09c4a2f402b0bbbb819305c644
narasu <narasu@users.noreply.github.com>
2020-06-16 18:53:13 +0200

    fish class

---
+++ Assets/Scripts/Fish/FishFSM.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishFSM
+{
+    public Fish owner { get; private set; }
+
+    private Dictionary<FishStateType, FishState> states;
+
+    public FishStateType CurrentStateType { get; private set; }
+    private FishState currentState;
+    private FishState previousState;
+
+    public void Initialize(Fish owner)
+    {
+        this.owner = owner;
+        Debug.Log(owner);
+        states = new Dictionary<FishStateType, FishState>();
+    }
+
+    public void AddState(FishStateType newType, FishState newState)
+    {
+        states.Add(newType, newState);
+        states[newType].Initialize(this);
+    }
+
+    public void UpdateState()
+    {
+        currentState?.Update();
+    }
+
+    public void GotoState(FishStateType key)
+    {
+        if (!states.ContainsKey(key))
+        {
+            return;
+        }
+
+        currentState?.Exit();
+
+        previousState = currentState;
+        CurrentStateType = key;
+        currentState = states[CurrentStateType];
+
+        currentState.Enter();
+    }
+
+    public FishState GetState(FishStateType type)
+    {
+        if (!states.ContainsKey(type))
+        {
+            return null;
+        }
+        return states[type];
+    }
+}
---
+++ Assets/Scripts/Fish/FishState.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FishStateType { Idle, Walking }
+
+public abstract class FishState
+{
+    protected FishFSM owner;
+    protected Fish fish;
+
+    public void Initialize(FishFSM owner)
+    {
+        this.owner = owner;
+        fish = owner.owner;
+    }
+
+    public abstract void Enter();
+    public abstract void Update();
+    public abstract void Exit();
+}
+
+public class IdleState : FishState
+{
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Update()
+    {
+        if (fish.forwardInput!=0 || fish.horizInput!=0)
+        {
+            fish.GotoWalking();
+        }
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
+
+public class WalkingState : FishState
+{
+    public override void Enter()
+    {
+        fish.footsteps.PlaySound();
+    }
+
+    public override void Update()
+    {
+        if(fish.forwardInput==0 && fish.horizInput==0)
+        {
+            fish.GotoIdle();
+        }
+        fish.Walk();
+        
+    }
+
+    public override void Exit()
+    {
+        fish.footsteps.StopSound();
+    }
+}
